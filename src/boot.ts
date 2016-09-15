c44b12529c781de5f30b44dfae2e26346c855b8f
Karel Hala <khala@redhat.com>
2016-09-15 11:44:33 +0200

    Add resolution to node and add loading of typings

---
+++ src/boot.ts
@@ -0,0 +1,5 @@
+///<reference path="tsd.d.ts"/>
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
